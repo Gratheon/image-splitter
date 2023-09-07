88d5c46b6a91d52f13abb24671ea130dc8084f57
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-09-08 00:31:03 +0300

    update config loading

---
+++ src/config/config.test.ts
@@ -0,0 +1,27 @@
+export default {
+    routerSignature: "",
+    sentryDsn: "",
+    schema_registry_url: '',
+    selfUrl: "",
+    yolo_v5_url: "",
+    models_frame_resources_url: "",
+    mysql: {
+        host: '',
+        port: '',
+        user: '',
+        password: '',
+        database: '',
+    },
+
+    aws: {
+        "bucket": "",
+        "key": "",
+        "secret": ""
+    },
+
+    "files_base_url": "",
+
+    jwt:{
+        privateKey: "",
+    }
+}
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,15 @@
-import dev from './config.dev';
-import prod from './config.prod';
+import test from './config.test';
 
 const config = {
-	dev,
-	prod
+	test
 };
 
-const mode = process.env.ENV_ID === 'dev' ? 'dev' : 'prod';
+let mode = process.env.ENV_ID;
+if(!process.env.ENV_ID) {
+	mode = 'test'
+} else {
+	config['dev'] = import('./config.dev');
+	config['prod'] = import('./config.prod');
+}
 
 export default config[mode];
