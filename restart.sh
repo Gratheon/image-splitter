cf96d4237fb7b33673e79bb43f1c4cd32fc6e282
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-09-30 18:01:19 +0300

    ...

--- restart.sh
+++ restart.sh
@@ -3,12 +3,11 @@ COMPOSE_PROJECT_NAME=gratheon docker-compose down
 
 rm -rf /www/image-splitter/app/
 
-git rev-parse --short HEAD > .version
-
 su www
 cd /www/image-splitter/
 nvm use
 npm i
 npm run build
+git rev-parse --short HEAD > .version
 
 COMPOSE_PROJECT_NAME=gratheon docker-compose up -d --build
