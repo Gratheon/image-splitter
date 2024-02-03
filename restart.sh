2408c16db807b0958b52898a9e85b728db12e931
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-02-04 00:17:50 +0200

    fix deploy

--- restart.sh
+++ restart.sh
@@ -3,6 +3,6 @@ COMPOSE_PROJECT_NAME=gratheon docker-compose down
 
 rm -rf /www/image-splitter/app/
 
-sudo -u www bash -c 'cd /www/image-splitter/ && source ~/.nvm/nvm.sh && nvm use && npm i && npm run build'
+# sudo -u www bash -c 'cd /www/image-splitter/ && source ~/.nvm/nvm.sh && nvm use && npm i && npm run build'
 
-COMPOSE_PROJECT_NAME=gratheon docker-compose up -d --build
+COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.yml up --build
