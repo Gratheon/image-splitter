08a0bf6a7d5b7c3ec1868bd6d31226f61f108a7b
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-03-28 10:40:43 +0200

    catch varroa detection failures

--- src/workers/detectVarroa.ts
+++ src/workers/detectVarroa.ts
@@ -13,30 +13,36 @@ export async function detectVarroa(partialFilePath, cutPosition: CutPosition, sp
 		encoding: "base64"
 	});
 
-	const rawResult = await axios({
-		method: "POST",
-		url: "https://detect.roboflow.com/varroa-gasbl/1",
-		params: {
-			api_key: config.roboflow.token
-		},
-		data: image,
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded"
-		}
-	});
-
-	logger.info('Converting JSON to more compact format');
 	const result: DetectedObject[] = [];
-
-	for (let row of rawResult.data.predictions) {
-		result.push({
-			n: '11',
-			x: roundToDecimal((Number(row.x) + cutPosition.left) / (splitCountX * cutPosition.width), 5),
-			y: roundToDecimal((Number(row.y) + cutPosition.top) / (splitCountY * cutPosition.height), 5),
-			w: roundToDecimal(Number(row.width) / (3 * cutPosition.width), 4),
-			h: roundToDecimal(Number(row.height) / (3 * cutPosition.height), 4),
-			c: roundToDecimal(row.confidence, 4)
+	
+	try {
+		const rawResult = await axios({
+			method: "POST",
+			url: "https://detect.roboflow.com/varroa-gasbl/1",
+			params: {
+				api_key: config.roboflow.token
+			},
+			data: image,
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded"
+			}
 		});
+
+		logger.info('Converting JSON to more compact format');
+		
+
+		for (let row of rawResult.data.predictions) {
+			result.push({
+				n: '11',
+				x: roundToDecimal((Number(row.x) + cutPosition.left) / (splitCountX * cutPosition.width), 5),
+				y: roundToDecimal((Number(row.y) + cutPosition.top) / (splitCountY * cutPosition.height), 5),
+				w: roundToDecimal(Number(row.width) / (3 * cutPosition.width), 4),
+				h: roundToDecimal(Number(row.height) / (3 * cutPosition.height), 4),
+				c: roundToDecimal(row.confidence, 4)
+			});
+		}
+	} catch (e) {
+		logger.error("detectVarroa failed");
 	}
 
 	return result;
