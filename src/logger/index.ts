86d9a7f35ae4b04b20f881cfb42fba1325f08ce1
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-05-26 22:54:34 +0300

    remove winston-mysql dependency, use single db adapter

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,12 +1,35 @@
 import config from "../config/index";
-import MySQLTransport from 'winston-mysql';
+import Transport from 'winston-transport';
 import winston, { transports, format } from 'winston';
+import createConnectionPool, { sql } from "@databases/mysql";
+
+class CustomMySQLLogTransport extends Transport {
+  conn: any;
+  constructor(opts) {
+    super(opts);
+
+    this.conn = createConnectionPool(
+      `mysql://${config.mysql.user}:${config.mysql.password}@${config.mysql.host}:${config.mysql.port}/logs`
+    );
+  }
+
+  log(info, callback) {
+    const { level, message, ...winstonMeta } = info;
+
+    // Perform the writing to the remote service
+    this.conn.query(sql`
+      INSERT INTO logs (level, message, meta, timestamp)
+      VALUES (${level}, ${message}, ${JSON.stringify(winstonMeta)}, NOW())
+    `);
+    callback();
+  }
+};
 
 export const logger = winston.createLogger({
   level: 'info',
   levels: Object.assign({ 'fatal': 0, 'warn': 4, 'trace': 7 }, winston.config.syslog.levels),
   format: format.json(),
-  defaultMeta: { service: 'image-splitter' },
+  defaultMeta: { service: 'user-cycle' },
   transports: [
     new transports.Console({
       format: format.combine(
@@ -16,7 +39,7 @@ export const logger = winston.createLogger({
       ),
       handleExceptions: true
     }),
-    new MySQLTransport({
+    new CustomMySQLLogTransport({
       host: config.mysql.host,
       user: config.mysql.user,
       password: config.mysql.password,
