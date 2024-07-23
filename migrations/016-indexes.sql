bbb2ea9ebc559068c603b52d69935f8b10a4005b
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-07-23 23:11:07 +0300

    add indexes

---
+++ migrations/016-indexes.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `files_frame_side_rel` ADD INDEX (`user_id`, `frame_side_id`, `inspection_id`);
+ALTER TABLE `files_frame_side_queen_cups` ADD INDEX (`user_id`, `frame_side_id`, `inspection_id`);
+ALTER TABLE `files_frame_side_cells` ADD INDEX (`user_id`, `frame_side_id`, `inspection_id`);
