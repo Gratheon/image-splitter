b58d023c9a0d8c7fd813f17678e505a71f94cf59
Artjom Kurapov <artkurapov@gmail.com>
2024-09-18 00:45:46 +0300

    fix redis publishing from DB

--- src/workers/redisNotifier.ts
+++ src/workers/redisNotifier.ts
@@ -2,9 +2,9 @@ import { logger } from "../logger";
 import { publisher } from "../redisPubSub";
 
 export default async function notifyViaRedis(ref_id, payload) {
-  logger.info("Publishing to redis channel " + payload.channel);
+  logger.info("Publishing to redis channel " + payload.redisChannelName);
   await publisher().publish(
-    payload.channel,
+    payload.redisChannelName,
     JSON.stringify({
       payload,
     }),
