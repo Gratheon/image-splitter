c9fc8e846d8c2c142916e7fcbee5115e20ab6a4f
Artjom Kurapov <artkurapov@gmail.com>
2024-12-21 23:08:05 +0200

    format code, add integration test for upload

--- src/image-splitter.ts
+++ src/image-splitter.ts
@@ -1,126 +1,123 @@
-import { ApolloServer } from "apollo-server-fastify";
-import {
-  ApolloServerPluginDrainHttpServer,
-  ApolloServerPluginLandingPageGraphQLPlayground,
-} from "apollo-server-core";
+import {ApolloServer} from "apollo-server-fastify";
+import {ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageGraphQLPlayground,} from "apollo-server-core";
 import fastify from "fastify";
-import { buildSubgraphSchema } from "@apollo/federation";
-import { processRequest } from "graphql-upload";
+import {buildSubgraphSchema} from "@apollo/federation";
+import {processRequest} from "graphql-upload";
 import jwt from "jsonwebtoken";
 import gql from "graphql-tag";
 
 import orchestrator from "./workers/orchestrator";
-import { schema } from "./graphql/schema";
-import { resolvers } from "./graphql/resolvers";
-import { initStorage } from "./models/storage";
-import { registerSchema } from "./graphql/schema-registry";
+import {schema} from "./graphql/schema";
+import {resolvers} from "./graphql/resolvers";
+import {initStorage} from "./models/storage";
+import {registerSchema} from "./graphql/schema-registry";
 import config from "./config/index";
-import { logger, fastifyLogger } from "./logger";
+import {fastifyLogger, logger} from "./logger";
 import "./sentry";
 
 function fastifyAppClosePlugin(app) {
-  return {
-    async serverWillStart() {
-      return {
-        async drainServer() {
-          await app.close();
+    return {
+        async serverWillStart() {
+            return {
+                async drainServer() {
+                    await app.close();
+                },
+            };
         },
-      };
-    },
-  };
+    };
 }
 
 async function startApolloServer(app, typeDefs, resolvers) {
-  app.addContentTypeParser("multipart", (request, payload, done) => {
-    request.isMultipart = true;
-    done();
-  });
+    app.addContentTypeParser("multipart", (request, payload, done) => {
+        request.isMultipart = true;
+        done();
+    });
 
-  // Format the request body to follow graphql-upload's
-  app.addHook("preValidation", async function (request, reply) {
-    if (!request.isMultipart) {
-      return;
-    }
+    // Format the request body to follow graphql-upload's
+    app.addHook("preValidation", async function (request, reply) {
+        if (!request.isMultipart) {
+            return;
+        }
 
-    request.body = await processRequest(request.raw, reply.raw);
-  });
+        request.body = await processRequest(request.raw, reply.raw);
+    });
 
-  const server = new ApolloServer({
-    schema: buildSubgraphSchema({ typeDefs: gql(typeDefs), resolvers }),
+    const server = new ApolloServer({
+        schema: buildSubgraphSchema({typeDefs: gql(typeDefs), resolvers}),
 
-    plugins: [
-      fastifyAppClosePlugin(app),
-      ApolloServerPluginLandingPageGraphQLPlayground(),
-      ApolloServerPluginDrainHttpServer({ httpServer: app.server }),
-    ],
-    // @ts-ignore
-    uploads: {
-      maxFileSize: 30000000, // 30 MB
-      maxFiles: 20,
-    },
-    context: async (req) => {
-      let uid;
-      let signature = req.request.raw.headers["internal-router-signature"];
+        plugins: [
+            fastifyAppClosePlugin(app),
+            ApolloServerPluginLandingPageGraphQLPlayground(),
+            ApolloServerPluginDrainHttpServer({httpServer: app.server}),
+        ],
+        // @ts-ignore
+        uploads: {
+            maxFileSize: 30000000, // 30 MB
+            maxFiles: 20,
+        },
+        context: async (req) => {
+            let uid;
+            let signature = req.request.raw.headers["internal-router-signature"];
 
-      // signature sent by router so that it cannot be faked
-      if (signature === config.routerSignature) {
-        uid = req.request.raw.headers["internal-userid"];
-      }
-
-      // allow direct access in case of upload
-      else {
-        const token = req.request.raw.headers.token;
-        const decoded = (await new Promise((resolve, reject) =>
-          jwt.verify(token, config.jwt.privateKey, function (err, decoded) {
-            if (err) {
-              reject(err);
+            // signature sent by router so that it cannot be faked
+            if (signature === config.routerSignature) {
+                uid = req.request.raw.headers["internal-userid"];
             }
-            resolve(decoded);
-          }),
-        )) as {
-          user_id: string;
-        };
 
-        uid = decoded?.user_id;
-      }
+            // allow direct access in case of upload
+            else {
+                const token = req.request.raw.headers.token;
+                const decoded = (await new Promise((resolve, reject) =>
+                    jwt.verify(token, config.jwt.privateKey, function (err, decoded) {
+                        if (err) {
+                            reject(err);
+                        }
+                        resolve(decoded);
+                    }),
+                )) as {
+                    user_id: string;
+                };
 
-      return {
-        uid,
-      };
-    },
-  });
+                uid = decoded?.user_id;
+            }
 
-  await server.start();
-  app.register(server.createHandler());
+            return {
+                uid,
+            };
+        },
+    });
 
-  return server.graphqlPath;
+    await server.start();
+    app.register(server.createHandler());
+
+    return server.graphqlPath;
 }
 
 (async function main() {
-  logger.info("Starting service...");
+    logger.info("Starting service...");
 
-  await initStorage(logger);
-  orchestrator();
+    await initStorage(logger);
+    orchestrator();
 
-  const app = fastify({
-    logger: fastifyLogger,
-  });
+    const app = fastify({
+        logger: fastifyLogger,
+    });
 
-  try {
-    let schemaString = schema();
+    try {
+        let schemaString = schema();
 
-    // no need to register schema in integration test mode
-    if (process.env.ENV_ID != "testing") {
-      await registerSchema(schemaString);
+        // no need to register schema in integration test mode
+        if (process.env.ENV_ID != "testing") {
+            await registerSchema(schemaString);
+        }
+
+        const relPath = await startApolloServer(app, schemaString, resolvers);
+        await app.listen(8800, "0.0.0.0");
+
+        logger.info(
+            `image-splitter service is ready at http://localhost:8800${relPath}`,
+        );
+    } catch (e) {
+        logger.error(e);
     }
-
-    const relPath = await startApolloServer(app, schemaString, resolvers);
-    await app.listen(8800, "0.0.0.0");
-
-    logger.info(
-      `image-splitter service is ready at http://localhost:8800${relPath}`,
-    );
-  } catch (e) {
-    logger.error(e);
-  }
 })();
--- test/integration/hello.test.ts
+++ test/integration/hello.test.ts
@@ -1,5 +1,7 @@
+import * as fs from "fs";
 import {expect} from '@jest/globals';
 import fetch from 'node-fetch';
+import config from '../../src/config';
 
 // port from docker-compose.test.yml
 const URL = 'http://localhost:8800/graphql';
@@ -8,7 +10,7 @@ describe('POST /graphql', () => {
     beforeEach(() => {
     });
 
-    it('clientsModel.add() should fill internalCache', async () => {
+    it.skip('hello_image_splitter', async () => {
         // make POST request
         // Send a POST request to the API endpoint
         const response = await fetch(URL, {
@@ -34,5 +36,46 @@ describe('POST /graphql', () => {
         expect(result?.data).toEqual({
             hello_image_splitter: 'hi',
         });
+    });
+
+
+    it('uploadFrameSide', async () => {
+        const filePath = './test/integration/fixture/IMG_4368.JPG';
+        const fileBuffer = fs.readFileSync(filePath);
+        const fileName = filePath.split('/').pop();
+
+        const form = new FormData();
+        form.append('operations', JSON.stringify({
+            query: `
+                mutation($file: Upload!) {
+                    uploadFrameSide(file: $file) {
+                        __typename
+                        id
+                        url
+                    }
+                }
+            `,
+            variables: { file: null }
+        }));
+        form.append('map', JSON.stringify({ "0": ["variables.file"] }));
+        form.append('0', new Blob([fileBuffer]), fileName);
+
+        const response = await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'internal-router-signature': config.routerSignature,
+                'internal-userid' : '1',
+            },
+            body: form
+        });
+
+        console.log('Response:', response);
+        expect(response.status).toBe(200);
+        const result = await response.json();
+
+        expect(result).toHaveProperty('data');
+        expect(result.data).toHaveProperty('uploadFrameSide');
+        expect(result.data.uploadFrameSide).toHaveProperty('id');
+        expect(result.data.uploadFrameSide).toHaveProperty('url');
     });
 });
