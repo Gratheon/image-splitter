79cdd9fb5ec9463b9e45ea7258e1f1fcf409fdf4
Artjom Kurapov <artkurapov@gmail.com>
2025-04-06 19:11:47 +0300

    ...

--- test/init-db.sql
+++ test/init-db.sql
@@ -1,5 +1,5 @@
--- Grant privileges to the 'test' user on the 'logs' database.
--- Note: The 'test' user and 'image-splitter' database are created by environment variables.
--- The 'logs' database needs to be added to MYSQL_DATABASE env var as well.
+-- Grant privileges to the 'test' user on BOTH databases.
+-- The user and databases are created by environment variables in docker-compose.test.yml
+GRANT ALL PRIVILEGES ON `image-splitter`.* TO 'test'@'%';
 GRANT ALL PRIVILEGES ON `logs`.* TO 'test'@'%';
 FLUSH PRIVILEGES;
