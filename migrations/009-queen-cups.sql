50b61f4ef1d519e230f7821d3ded9d655283f6bc
Artjom Kurapov <artkurapov@gmail.com>
2025-12-24 01:10:05 +0200

    Revert "fix migration in digital ocean"
    This reverts commit 17559ba60df6be59d948f576647f05bd6aa033db.

--- migrations/009-queen-cups.sql
+++ migrations/009-queen-cups.sql
@@ -1,5 +1,3 @@
-SET SESSION sql_require_primary_key = 0;
-
 CREATE TABLE IF NOT EXISTS `files_frame_side_queen_cups` (
   `frame_side_id` int unsigned NOT NULL,
   `file_id` int unsigned NOT NULL,
@@ -7,11 +5,8 @@ CREATE TABLE IF NOT EXISTS `files_frame_side_queen_cups` (
   `cups` json DEFAULT NULL,
   `process_start_time` datetime DEFAULT NULL,
   `process_end_time` datetime DEFAULT NULL,
-  `added_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`frame_side_id`, `file_id`)
+  `added_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-SET SESSION sql_require_primary_key = 1;
 
 INSERT INTO `files_frame_side_queen_cups` (
   frame_side_id,
--- migrations/018-box-files.sql
+++ migrations/018-box-files.sql
@@ -1,14 +1,9 @@
-SET SESSION sql_require_primary_key = 0;
-
 CREATE TABLE `files_box_rel` (
   `box_id` int unsigned NOT NULL,
   `file_id` int unsigned NOT NULL,
   `user_id` int unsigned NOT NULL,
   `inspection_id` INT NULL DEFAULT NULL,
   `added_time` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`box_id`, `file_id`),
   INDEX (`user_id`, `box_id`, `inspection_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-SET SESSION sql_require_primary_key = 1;
 
